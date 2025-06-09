aa538c07d77903b5838200b6388f7511cd17248b
sharp-circles <sharpcirclesorg@gmail.com>
2025-06-09 19:05:20 +0200

    Insecure data layer implementation & snapshots

---
+++ InsecureDataLayerImplementation/owasp10.A03.interactors/UseCases/MedicalHistory/GetUserMedicalHistoryRecords/Contracts/IGetUserMedicalHistoryRecordsInteractor.cs
@@ -0,0 +1,8 @@
+﻿using owasp10.A03.business.UseCases.GetUserMedicalHistoryRecords.Dtos;
+
+namespace owasp10.A03.business.UseCases.GetUserMedicalHistoryRecords.Contracts;
+
+public interface IGetUserMedicalHistoryRecordsInteractor
+{
+    Task<IEnumerable<GetUserMedicalHistoryRecordsOutput>> Handle(GetUserMedicalHistoryRecordsInput getUserMedicalHistoryRecordsInput);
+}
